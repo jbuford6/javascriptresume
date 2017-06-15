d2ffa3cf2d637f0ce40f8a02230050ba3c4a180c
InnovateBham_5 <InnovateBham_5@UABiLab-5s-iMac.local>
2017-06-15 17:09:43 -0500

    initial commit

---
+++ resume.js
@@ -0,0 +1,41 @@
+var name = "Name: Jamaris Buford";
+var career = "Career: Developer";
+var description = "Descripton: I am a developer and I'm getting better everyday";
+var interests = "My Interests:";
+var interests1 = "* Computers";
+var interests2 = "* Sports";
+var interests3 = "* Fitness";
+var interests4 = "* Money";
+var experience = "My Previous Experience:";
+var skills = "My Skills:";
+
+
+
+function displayPosition(companyName,jobTitle,description) {
+    return "* " + companyName + jobTitle + description;
+}
+
+function displaySkill()
+    
+    
+
+
+console.log(name);
+console.log(career);
+console.log(description);
+console.log(interests);
+console.log(interests1);
+console.log(interests2);
+console.log(interests3);
+console.log(interests4);
+console.log(experience)
+console.log(displayPosition("Vestavia Hills Highschool, ", "Football ", "- Cornerback on defense."));
+console.log(displayPosition("Donohoo Auto, ", "Porter ", "- Parked and delivered cars."));
+console.log(displayPosition("Western Supermarket, ", "Stocker ", "- Stocked items and kept inventory."));
+console.log(skills);
+
+
+
+
+
+
